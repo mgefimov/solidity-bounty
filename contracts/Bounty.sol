ca02926af2ba46bbfa3eb48db2f379a9686368c6
Maksim Efimov <mg.efimov97@gmail.com>
2021-11-29 18:42:05 +0300

    smart contract and tests

---
+++ contracts/Bounty.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bounty {
+  struct Task {
+    uint[][]graph;
+    uint prize;
+    uint id;
+  }
+
+  uint public count = 0;
+  mapping (uint => Task) public bounties;
+
+  function addBounty(uint [][]calldata graph) external payable {
+    require(msg.value > 0);
+    count++;
+    uint id = count;
+    Task storage newTask = bounties[id];
+    for (uint i=0;i<graph.length;i++) {
+      newTask.graph.push(graph[i]);
+    }
+    newTask.prize = msg.value;
+    newTask.id = id;
+  }
+
+  function getGraph(uint id) external view returns(uint[][] memory) {
+    return bounties[id].graph;
+  }
+
+  function presentSolution(uint id, uint[] calldata solution) external {
+    Task storage task = bounties[id];
+    require(task.prize > 0, "Task not found");
+    require(check(id, solution), "Incorrect solution");
+    uint prize = task.prize;
+    delete bounties[id];
+    payable(msg.sender).transfer(prize);
+  }
+
+  function check(uint id, uint[] calldata solution) private view returns (bool) {
+    Task storage task = bounties[id];
+    bool[] memory used = new bool[](task.graph.length);
+    uint prev = solution[0];
+    for(uint i=1;i<solution.length - 1;i++){
+      used[prev] = true;
+      uint nxt = solution[i];
+      if (contains(task.graph[prev], nxt) && !used[nxt]) {
+        prev = nxt;
+      } else {
+        return false;
+      }
+    }
+    used[prev] = true;
+    for(uint i=0;i<task.graph.length;i++){
+      if (!used[i]) {
+        return false;
+      }
+    }
+    return solution[solution.length - 1] == solution[0];
+  }
+
+  function contains(uint[]storage arr, uint el) private view returns (bool) {
+    for(uint i=0;i<arr.length;i++){
+      if (el == arr[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ test/testBounty.test.js
@@ -0,0 +1,89 @@
+const Bounty = artifacts.require("Bounty");
+
+contract("Bounty", (accounts) => {
+  const graph = [
+    [1, 2],
+    [0, 2],
+    [0, 1],
+  ];
+  let instance = null;
+
+  before(async () => {
+    instance = await Bounty.deployed();
+    await instance.addBounty.sendTransaction(graph, {
+      from: accounts[0],
+      value: web3.utils.toWei("5", "ether"),
+    });
+  });
+
+  it("Проверяем что задача успешно создалась", async () => {
+    const task1 = await instance.bounties.call(1);
+    assert.equal(task1.id.toNumber(), 1);
+    assert.equal(web3.utils.fromWei(task1.prize, "ether"), "5");
+
+    let graph2 = await instance.getGraph.call(1);
+    graph2 = graph2.map((l) => l.map((bn) => bn.toNumber()));
+    expect(graph2).to.eql(graph);
+  });
+
+  it("Проверяем что задачу нельзя решить с неправильным решением", async () => {
+    const incorrectSolutions = [
+      [0, 1, 3, 0],
+      [0, 1, 0],
+      [],
+      [0, 1, 0, 1, 2, 0],
+      [0],
+      [1],
+      [0, 1, 2, 0, 0],
+    ];
+    for (const incorrectSolution of incorrectSolutions) {
+      let ok = false;
+      try {
+        await instance.presentSolution.sendTransaction(1, incorrectSolution, {
+          from: accounts[1],
+        });
+      } catch (error) {
+        ok = true;
+      }
+      assert.ok(ok);
+    }
+  });
+
+  it("Проверяем что задача успено решается", async () => {
+    const correctSolution = [0, 1, 2, 0];
+    const balance = web3.utils.fromWei(
+      await web3.eth.getBalance(accounts[1]),
+      "ether"
+    );
+    await instance.presentSolution.sendTransaction(1, correctSolution, {
+      from: accounts[1],
+    });
+    const newBalance = web3.utils.fromWei(
+      await web3.eth.getBalance(accounts[1]),
+      "ether"
+    );
+    const balanceDiff = newBalance - balance;
+    // Проверяем что наш баланс увеличивается
+    assert.ok(balanceDiff > 4.9);
+
+    // Проверяем что задача корректно удаляется
+    let graph2 = await instance.getGraph.call(1);
+    expect(graph2).to.eql([]);
+    const task1 = await instance.bounties.call(1);
+    assert.equal(task1.id.toNumber(), 0);
+    assert.equal(web3.utils.fromWei(task1.prize, "ether"), "0");
+  });
+
+  it("Проверяем что задачу нельзя решить дважды", async () => {
+    const correctSolution = [0, 1, 2, 0];
+    let ok = false;
+    try {
+      await instance.presentSolution.sendTransaction(1, correctSolution, {
+        from: accounts[1],
+      });
+    } catch (error) {
+      ok = true;
+    }
+    assert.ok(ok);
+  });
+});
